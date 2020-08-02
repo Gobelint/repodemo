38a13c907987ad9a854594bbc04317589b340932
jamal <jamal210free@hotmal.com>
2020-08-02 18:26:38 -0500

    cambios en v1

--- index.php
+++ index.php
@@ -11,3 +11,6 @@ hola umundo
 </body>
 </html>
 >>>>>>> v1
+
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam rerum fuga mollitia magni voluptas aliquam repudiandae minus. Mollitia maiores eligendi libero nesciunt hic quam minus, laudantium facilis ipsum quia amet.
